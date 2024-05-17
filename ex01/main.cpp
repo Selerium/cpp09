6d721275e7305e1d6ea9086034c7bd320f29aad8
John Adithya <jadithya@lab3r7s10.42abudhabi.ae>
2024-05-17 15:23:12 +0400

    fixed ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,6 +4,7 @@
 
 int main (int argc, char **argv) {
 	RPN calculator;
+	int nums = 0, ops = 0;
 	std::string c;
 	std::string numbers = "01234567890";
 	std::string operators = "+-*/";
@@ -29,6 +30,18 @@ int main (int argc, char **argv) {
 					return 1;
 				}
 			}
+		}
+		for (size_t i = 0; i < c.length(); i++) {
+			if (i % 2 == 0) {
+				if (numbers.find(c[i]) != std::string::npos)
+					nums++;
+				else if (operators.find(c[i]) != std::string::npos)
+					ops++;
+			}
+		}
+		if (nums != ops + 1) {
+			std::cout << "Error: incorrect expression" << std::endl;
+			return 1;
 		}
 		std::cout << calculator.getList().top() << std::endl;
 	}
